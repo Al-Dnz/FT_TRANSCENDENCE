c9c1661201ea570023e9328993d0b6f2ab521c3d
BOYER_Damien <damien.boyer@astrachain.com>
2022-11-01 23:22:57 +0100

    fix: front works as expected behind nginx proxy

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,148 +1,130 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    name : 'log',
-          path: '/',
-          components: 
     {
-      logpage: () => import('@/views/logscreen.vue'),
-    }
-      },
-      {
-          path: '/home',
-          components: 
-    {
-      default: () => import('@/views/Home.vue'),
-      navbar:	()=> import('@/components/Nav.vue'),
-      headbar: ()=> import('@/components/Header.vue')
+        name: "log",
+        path: "/",
+        components: {
+            logpage: () => import("@/views/logscreen.vue"),
+        },
     },
-    props : 
     {
-      headbar: {
-        sectionTitle: "Home",
-        type: String
-      }
-    }
-      },
-  {
-    path: '/user/:id',
-    components: 
-    {
-      default: () => import('@/views/Account.vue'),
-      navbar:	()=> import('@/components/Nav.vue'),
-      headbar: ()=> import('@/components/Header.vue')
+        path: "/home",
+        components: {
+            default: () => import("@/views/Home.vue"),
+            navbar: () => import("@/components/Nav.vue"),
+            headbar: () => import("@/components/Header.vue"),
+        },
+        props: {
+            headbar: {
+                sectionTitle: "Home",
+                type: String,
+            },
+        },
     },
-    props : 
     {
-      headbar: {
-        sectionTitle: "Account",
-        type: String
-      }
-    }
-  },
-  {
-    path: '/chat',
-    components: 
-    {
-      default: () => import('@/views/Chat.vue'),
-      navbar:	()=> import('@/components/Nav.vue'),
-      headbar: ()=> import('@/components/Header.vue')
+        path: "/user/:id",
+        components: {
+            default: () => import("@/views/Account.vue"),
+            navbar: () => import("@/components/Nav.vue"),
+            headbar: () => import("@/components/Header.vue"),
+        },
+        props: {
+            headbar: {
+                sectionTitle: "Account",
+                type: String,
+            },
+        },
     },
-    props : 
     {
-      headbar: {
-        sectionTitle: "Chat",
-        type: String
-      }
-    }
-  },
-  {
-    path: '/chat/:id',
-    components: 
-    {
-      default: () => import('@/views/Chat.vue'),
-      navbar:	()=> import('@/components/Nav.vue'),
-      headbar: ()=> import('@/components/Header.vue')
+        path: "/chat",
+        components: {
+            default: () => import("@/views/Chat.vue"),
+            navbar: () => import("@/components/Nav.vue"),
+            headbar: () => import("@/components/Header.vue"),
+        },
+        props: {
+            headbar: {
+                sectionTitle: "Chat",
+                type: String,
+            },
+        },
     },
-    props : 
     {
-      headbar: {
-        sectionTitle: "Chat",
-        type: String
-      }
-    }
-  },
-  {
-    path: '/friendList',
-    components: 
-    {
-      default: () => import('@/views/Friendlist.vue'),
-      navbar:	()=> import('@/components/Nav.vue'),
-      headbar: ()=> import('@/components/Header.vue')
+        path: "/chat/:id",
+        components: {
+            default: () => import("@/views/Chat.vue"),
+            navbar: () => import("@/components/Nav.vue"),
+            headbar: () => import("@/components/Header.vue"),
+        },
+        props: {
+            headbar: {
+                sectionTitle: "Chat",
+                type: String,
+            },
+        },
     },
-    props : 
     {
-      headbar: {
-        sectionTitle: "Friendlist",
-        type: String
-      }
-    }
-  },
-  {
-    path: '/spectate',
-    components: 
-    {
-      default: () => import('@/views/Spectate.vue'),
-      navbar:	()=> import('@/components/Nav.vue'),
-      headbar: ()=> import('@/components/Header.vue')
+        path: "/friendList",
+        components: {
+            default: () => import("@/views/Friendlist.vue"),
+            navbar: () => import("@/components/Nav.vue"),
+            headbar: () => import("@/components/Header.vue"),
+        },
+        props: {
+            headbar: {
+                sectionTitle: "Friendlist",
+                type: String,
+            },
+        },
     },
-    props : 
     {
-      headbar: {
-        sectionTitle: "Spectate",
-        type: String
-      }
-    }
-  },
-  {
-    path: '/blocked',
-    components: 
-    {
-      default: () => import('@/views/BlockList.vue'),
-      navbar:	()=> import('@/components/Nav.vue'),
-      headbar: ()=> import('@/components/Header.vue')
+        path: "/spectate",
+        components: {
+            default: () => import("@/views/Spectate.vue"),
+            navbar: () => import("@/components/Nav.vue"),
+            headbar: () => import("@/components/Header.vue"),
+        },
+        props: {
+            headbar: {
+                sectionTitle: "Spectate",
+                type: String,
+            },
+        },
     },
-    props : 
     {
-      headbar: {
-        sectionTitle: "BlockedList",
-        type: String
-      }
-    }
-  },
-  {
-    path: '/param',
-    components: 
-    {
-      default: () => import('@/views/Param.vue'),
-      navbar:	()=> import('@/components/Nav.vue'),
-      headbar: ()=> import('@/components/Header.vue')
+        path: "/blocked",
+        components: {
+            default: () => import("@/views/BlockList.vue"),
+            navbar: () => import("@/components/Nav.vue"),
+            headbar: () => import("@/components/Header.vue"),
+        },
+        props: {
+            headbar: {
+                sectionTitle: "BlockedList",
+                type: String,
+            },
+        },
     },
-    props : 
     {
-      headbar: {
-        sectionTitle: "parametres",
-        type: String
-      }
-    }
-  }
-]
-
+        path: "/param",
+        components: {
+            default: () => import("@/views/Param.vue"),
+            navbar: () => import("@/components/Nav.vue"),
+            headbar: () => import("@/components/Header.vue"),
+        },
+        props: {
+            headbar: {
+                sectionTitle: "parametres",
+                type: String,
+            },
+        },
+    },
+];
+const baseURL = process.env.NODE_ENV === "production" ? "/app" : "/";
 const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
+    history: createWebHistory(baseURL),
+    routes,
+});
 
-export default router
+export default router;
