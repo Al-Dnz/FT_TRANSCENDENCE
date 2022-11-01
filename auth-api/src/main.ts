9d1753f07133f043699acfa297dfdd842bc2e7e6
BOYER_Damien <damien.boyer@astrachain.com>
2022-11-01 15:27:20 +0100

    feat: add auth-api endpoints in swagger-ui

--- auth-api/src/main.ts
+++ auth-api/src/main.ts
@@ -1,9 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { BadRequestException, ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { HttpExceptionFilter } from './http-exception.filters';
-import { ValidationError } from 'class-validator';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { BadRequestException, ValidationPipe } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { HttpExceptionFilter } from "./http-exception.filters";
+import { ValidationError } from "class-validator";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -12,9 +12,9 @@ async function bootstrap() {
     app.useGlobalPipes(
         new ValidationPipe({
             exceptionFactory: (errors: ValidationError[]): any => {
-                return new BadRequestException(errors.join('; ').replace(/\n/g, ''));
+                return new BadRequestException(errors.join("; ").replace(/\n/g, ""));
             },
-        }),
+        })
     );
 
     // Custom http error handler
@@ -22,13 +22,13 @@ async function bootstrap() {
 
     // Setup swagger module
     const config = new DocumentBuilder()
-        .setTitle('Oauth-api')
-        .setDescription('Api which handle the oauth authentication')
-        .setVersion('1.0')
-        .addTag('oauth')
+        .setTitle("Oauth-api")
+        .setDescription("Api which handle the oauth authentication")
+        .setVersion("1.0")
+        .addTag("oauth")
         .build();
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+    SwaggerModule.setup("api", app, document);
 
     await app.listen(3000);
 }
