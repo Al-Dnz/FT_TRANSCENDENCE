cebad7d887856cd13184009d54bc7f4bc870c990
BOYER_Damien <damien.boyer@astrachain.com>
2022-11-06 19:43:02 +0100

    fix: adapt db entity for user-api

--- db-interface/src/entity/Core.ts
+++ db-interface/src/entity/Core.ts
@@ -9,6 +9,8 @@ import {
     OneToOne,
     JoinColumn,
     UpdateDateColumn,
+    Relation,
+    PrimaryColumn,
 } from "typeorm";
 
 import { Min } from "class-validator";
@@ -56,116 +58,37 @@ export enum ChannelType {
  **********************************************************************************************/
 
 @Entity()
-export class User extends Base {
-    @Index("login-idx")
-    @Column({ unique: true })
-    login: string;
-
-    @Index("username-idx")
-    @Column({ unique: true })
-    userName: string;
-
-    @Column("boolean", { default: false })
-    twoFa: boolean;
-
-    @Column({ type: "enum", enum: UserStatus, default: UserStatus.offline })
-    status: UserStatus;
-
-    @ManyToMany(() => User, { cascade: true })
-    @JoinTable()
-    friends: User[];
-
-    @ManyToMany(() => User, { cascade: true })
-    @JoinTable()
-    blockedUser: User[];
-
-    @OneToMany(() => Avatar, (avatar: Avatar) => avatar.user, {
-        cascade: true,
-    })
-    avatars: Avatar[];
-
-    @OneToOne(() => Avatar, { onDelete: "SET NULL", nullable: true })
-    @JoinColumn()
-    actualAvatar: Avatar;
-
-    @OneToOne(() => UserStats, (stats: UserStats) => stats.user, {
-        onDelete: "RESTRICT",
-    })
-    @JoinColumn()
-    stats: UserStats;
-
-    @OneToMany(() => UserMatch, (userMatch: UserMatch) => userMatch.user, {
-        cascade: true,
-    })
-    userMatchs: UserMatch[];
-
-    @OneToMany(
-        () => UserChannel,
-        (userChannel: UserChannel) => userChannel.user,
-        {
-            cascade: true,
-        }
-    )
-    userChannels: UserChannel[];
-}
-
-@Entity()
-export class Avatar extends Base {
-    @Index("path-idx")
-    @Column({ unique: true })
-    path: string;
-
-    @ManyToOne(() => User, (user: User) => user.avatars, { onDelete: "CASCADE" })
-    user: User;
-}
-
-@Entity()
-export class UserStats extends Base {
-    @Column()
-    @Min(0)
-    level: number;
-
-    @Column()
-    @Min(0)
-    victories: number;
-
-    @Column()
-    @Min(0)
-    defeats: number;
-
-    @OneToOne(() => User, (user: User) => user.stats, { onDelete: "CASCADE" })
-    user: User;
-}
-
-@Entity()
 export class Match extends Base {
     @OneToOne(() => UserMatch, (userMatch: UserMatch) => userMatch.match)
-    userMatch: UserMatch;
+    userMatch: Relation<UserMatch>;
 
     @Column({ type: "date" })
     finishedAt: Date;
 
     @OneToOne(() => UserMatch, { onDelete: "SET NULL", nullable: true })
-    winner: UserMatch;
+    winner: Relation<UserMatch>;
 }
 
 @Entity()
-export class UserMatch extends Base {
-    @ManyToOne(() => User, (user: User) => user.userMatchs, {
+export class UserChannel extends Base {
+    @Column()
+    role: UserChannelRole;
+
+    @Column()
+    status: UserChannelStatus;
+
+    @ManyToOne(() => Channel, (channel: Channel) => channel.members, {
         onDelete: "CASCADE",
     })
-    user: User;
+    channel: Relation<Channel>;
 
-    @OneToOne(() => Match, { onDelete: "CASCADE" })
-    @JoinColumn()
-    match: Match;
+    @ManyToOne(() => User, (user: User) => user.userChannels, {
+        onDelete: "CASCADE",
+    })
+    user: Relation<User>;
 
-    @Column()
-    @Min(0)
-    score: number;
-
-    @Column()
-    role: UserMatchRole;
+    @OneToOne(() => UserChannel)
+    mutedBy: UserChannel;
 }
 
 @Entity()
@@ -192,28 +115,6 @@ export class Channel extends Base {
 }
 
 @Entity()
-export class UserChannel extends Base {
-    @Column()
-    role: UserChannelRole;
-
-    @Column()
-    status: UserChannelStatus;
-
-    @ManyToOne(() => Channel, (channel: Channel) => channel.members, {
-        onDelete: "CASCADE",
-    })
-    channel: Channel;
-
-    @ManyToOne(() => User, (user: User) => user.userChannels, {
-        onDelete: "CASCADE",
-    })
-    user: User;
-
-    @OneToOne(() => UserChannel)
-    mutedBy: UserChannel;
-}
-
-@Entity()
 export class Message extends Base {
     @UpdateDateColumn()
     updatedAt: Date;
@@ -228,4 +129,119 @@ export class Message extends Base {
         onDelete: "CASCADE",
     })
     channel: Channel;
+}
+
+@Entity()
+export class Avatar extends Base {
+    @Column({ unique: true })
+    path: string;
+
+    @ManyToOne(() => User, { onDelete: "CASCADE" })
+    user: Relation<User>;
+
+    @Column({ default: false })
+    activate: boolean;
+
+    constructor(path: string, activate?: boolean) {
+        super();
+        this.path = path;
+        this.activate = activate ? activate : false;
+    }
+}
+
+@Entity()
+export class UserStats extends Base {
+    @Column({ default: 0 })
+    @Min(0)
+    level: number;
+
+    @Column({ default: 0 })
+    @Min(0)
+    victories: number;
+
+    @Column({ default: 0 })
+    @Min(0)
+    defeats: number;
+
+    @OneToOne(() => User, { onDelete: "CASCADE" })
+    user: Relation<User>;
+}
+
+@Entity()
+export class User extends Base {
+    @PrimaryColumn()
+    login: string;
+
+    @Index("username-idx")
+    @Column({ unique: true })
+    userName: string;
+
+    @Column("boolean", { default: false })
+    twoFa: boolean;
+
+    @Column({ type: "enum", enum: UserStatus, default: UserStatus.offline })
+    status: UserStatus;
+
+    @ManyToMany(() => User, { cascade: true })
+    @JoinTable()
+    friends: User[];
+
+    @ManyToMany(() => User, { cascade: true })
+    @JoinTable()
+    blockedUser: User[];
+
+    @OneToMany(() => UserMatch, (userMatch: UserMatch) => userMatch.user, {
+        cascade: true,
+    })
+    userMatchs: UserMatch[];
+
+    @OneToMany(
+        () => UserChannel,
+        (userChannel: UserChannel) => userChannel.user,
+        {
+            cascade: true,
+        }
+    )
+    userChannels: UserChannel[];
+
+    @OneToOne(() => UserStats, (stats: UserStats) => stats.user, {
+        onDelete: "RESTRICT",
+        eager: true,
+        cascade: true,
+    })
+    @JoinColumn({ name: "userStatsId", referencedColumnName: "id" })
+    stats: UserStats;
+
+    @OneToMany(() => Avatar, (avatar: Avatar) => avatar.user, {
+        cascade: true,
+        eager: true,
+    })
+    avatars: Avatar[];
+
+    constructor(login: string) {
+        super();
+
+        this.login = login;
+        this.userName = login;
+        this.stats = new UserStats();
+    }
+}
+
+@Entity()
+export class UserMatch extends Base {
+    @ManyToOne(() => User, (user: User) => user.userMatchs, {
+        onDelete: "CASCADE",
+    })
+    user: User;
+
+    @OneToOne(() => Match, { onDelete: "CASCADE" })
+    @JoinColumn()
+    match: Match;
+
+    @Column({ default: 0 })
+    @Min(0)
+    score: number;
+
+    @Column()
+    role: UserMatchRole;
 }
