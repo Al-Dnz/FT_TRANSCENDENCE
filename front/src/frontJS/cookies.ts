4909b3ea92d1880fbe0bdcfac05f840ea848d2f5
BOYER_Damien <damien.boyer@astrachain.com>
2022-11-11 18:54:58 +0100

    fix: cookies

--- front/src/frontJS/cookies.ts
+++ front/src/frontJS/cookies.ts
@@ -2,52 +2,52 @@ import { OauthToken } from "@/api/models";
 import { AuthenticationApi } from "@/api/index";
 
 export function getCookie(name: string): string {
-  const value = `; ${document.cookie}`;
-  // eslint-disable-next-line
-  const parts: any = value.split(`; ${name}=`); // Fuck type script fuck Es lint A voir si on peux fix
-  if (parts.length === 2) {
-    return parts.pop().split(";").shift();
-  } else return "";
+    const value = `; ${document.cookie}`;
+    // eslint-disable-next-line
+    const parts: any = value.split(`; ${name}=`); // Fuck type script fuck Es lint A voir si on peux fix
+    if (parts.length === 2) {
+        return parts.pop().split(";").shift();
+    } else return "";
 }
 
 export function setCookie(name: string, value: string, expiration: Date) {
-  document.cookie = `${name}=${value}; expires=${expiration.toUTCString()}`;
+    document.cookie = `${name}=${value}; expires=${expiration.toUTCString()}; path=/`;
 }
 
 export async function getCredentials(): Promise<string> {
-  const accessToken: string = getCookie("trans_access");
-  const refreshToken: string = getCookie("trans_refresh");
+    const accessToken: string = getCookie("trans_access");
+    const refreshToken: string = getCookie("trans_refresh");
 
-  if (!refreshToken) {
-    location.href = "http://localhost";
-    return "";
-  }
-
-  if (!accessToken) {
-    new AuthenticationApi()
-      .refreshToken({
-        refreshTokenRequest: { token: refreshToken },
-      })
-      .then((value: OauthToken) => {
-        setAccessCookie(value.accessToken);
-        setRefreshCookie(value.refreshToken);
-        return value.accessToken;
-      })
-      .catch((error: Error) => {
+    if (!refreshToken) {
         location.href = "http://localhost";
-      });
-  }
-  return accessToken;
+        return "";
+    }
+
+    if (!accessToken) {
+        new AuthenticationApi()
+            .refreshToken({
+                refreshTokenRequest: { token: refreshToken },
+            })
+            .then((value: OauthToken) => {
+                setAccessCookie(value.accessToken);
+                setRefreshCookie(value.refreshToken);
+                return value.accessToken;
+            })
+            .catch(() => {
+                location.href = "http://localhost";
+            });
+    }
+    return accessToken;
 }
 
 export function addMinutes(date: Date, minutes: number) {
-  return new Date(date.getTime() + minutes * 60000);
+    return new Date(date.getTime() + minutes * 60000);
 }
 
 export function setAccessCookie(accessToken: string) {
-  setCookie("trans_access", accessToken, addMinutes(new Date(), 1));
+    setCookie("trans_access", accessToken, addMinutes(new Date(), 15));
 }
 
 export function setRefreshCookie(accessToken: string) {
-  setCookie("trans_refresh", accessToken, addMinutes(new Date(), 60));
+    setCookie("trans_refresh", accessToken, addMinutes(new Date(), 60));
 }
