13ec99e36fdcc50cfb3b631968106c7e27412855
Niels <niels@Air-de-Niels.(none)>
2022-11-11 14:34:49 +0100

    Valide merge

--- front/src/views/Callback.vue
+++ front/src/views/Callback.vue
@@ -13,7 +13,7 @@ export default defineComponent({
 			this.$toast("Error log", {styles: {backgroundColor: '#FF0000', color: '#FFFFFF',}},);
 			return;
 		}
-
+		
 		this.$cookies.set("trans_acces", this.$route.query.code, 60 * 15);
 		this.$cookies.set("trans_refresh", this.$route.query.code, 60 * 60);
 		this.$router.push('/param');
