fc4fe8be12d930a00ba41f6bd1230d902618cc8b
BOYER_Damien <damien.boyer@astrachain.com>
2022-11-12 15:51:14 +0100

    feat(user-api): add error handling system

--- user-api/src/user/user.controller.ts
+++ user-api/src/user/user.controller.ts
@@ -17,7 +17,6 @@ import {
     InternalServerErrorException,
 } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { QueryFilterDto } from 'validation/query.dto';
 import { User, Avatar, Match } from 'db-interface/Core';
@@ -27,7 +26,7 @@ import {
     AvatarOutputDto,
 } from 'user-api/dto/user-output.dto';
 import { Identity } from './user.decorator';
-import { DeleteResult } from 'typeorm';
+import { DeleteResult, UpdateResult } from 'typeorm';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { LoggingInterceptor } from 'src/auth/auth.interceptor';
 import { MatchOutputDto } from './dto/match-output';
@@ -45,30 +44,35 @@ export class UserController {
             .findAll(query)
             .then((users: User[]) =>
                 users.map((value: User) => new UserOutputDto(value)),
-            );
-    }
-
-    @Post()
-    async create(@Identity() user: Identity): Promise<UserOutputDto> {
-        return this.userService
-            .findOne(user.login)
-            .then(async (value?: User) => {
-                return value
-                    ? new UserOutputDto(value)
-                    : new UserOutputDto(
-                        await this.userService.create(user.login, user.image_url),
-                    );
-            })
+            )
             .catch((error: Error) => {
                 throw new InternalServerErrorException(error.message);
             });
     }
 
+    @Post()
+    async create(@Identity() user: Identity): Promise<UserOutputDto> {
+        const found: User | undefined = await this.userService.findOne(user.login);
+        if (!found) {
+            return this.userService
+                .create(user.login, user.image_url)
+                .then((value: User) => {
+                    return new UserOutputDto(value);
+                })
+                .catch((error: Error) => {
+                    throw new InternalServerErrorException(error.message);
+                });
+        }
+        return new UserOutputDto(found);
+    }
+
     @Get('me')
     async userMe(@Identity() user: Identity): Promise<UserOutputDto> {
-        return this.userService
-            .findOne(user.login)
-            .then((value: User) => new UserOutputDto(value));
+        const found: User | undefined = await this.userService.findOne(user.login);
+        if (!found) {
+            throw new NotFoundException(`user ${user.login} not found`);
+        }
+        return new UserOutputDto(found);
     }
 
     @Patch('me')
@@ -76,32 +80,39 @@ export class UserController {
         @Body() updateUserDto: UpdateUserDto,
         @Identity() user: Identity,
     ): Promise<UserOutputDto> {
-        return this.userService
-            .updateByLogin(user.login, updateUserDto)
-            .then(() =>
-                this.userService
-                    .findOne(user.login)
-                    .then((value: User) => new UserOutputDto(value)),
-            );
+        const result: UpdateResult = await this.userService.updateByLogin(
+            user.login,
+            updateUserDto,
+        );
+        if (result.affected && result.affected > 0) {
+            return this.userService
+                .findOne(user.login)
+                .then((user: User) => new UserOutputDto(user))
+                .catch((error: Error) => {
+                    throw new InternalServerErrorException(error.message);
+                });
+        }
+        throw new NotFoundException(`user ${user.login} not found`);
     }
 
     @Delete('me')
     @HttpCode(204)
     async remove(@Identity() user: Identity): Promise<void> {
-        return this.userService
-            .removeByLogin(user.login)
-            .then((value: DeleteResult) => {
-                if (value.affected != 1) {
-                    throw new NotFoundException(`user ${user.login} not found`);
-                }
-            });
+        const result: DeleteResult = await this.userService.removeByLogin(
+            user.login,
+        );
+        if (!result.affected || (result.affected && result.affected == 0)) {
+            throw new NotFoundException(`user ${user.login} not found`);
+        }
     }
 
     @Get(':login')
     async findOne(@Param('login') login: string): Promise<UserOutputDto> {
-        return this.userService
-            .findOne(login)
-            .then((value: User) => new UserOutputDto(value));
+        const user: User | undefined = await this.userService.findOne(login);
+        if (user) {
+            return new UserOutputDto(user);
+        }
+        throw new NotFoundException(`user ${login} not found`);
     }
 
     @Get('me/friends')
@@ -109,11 +120,8 @@ export class UserController {
         @Identity() user: Identity,
         @Query() query: QueryFilterDto,
     ): Promise<UserOutputDto[]> {
-        return this.userService
-            .findFriends(user.login, query)
-            .then((value: User[]) =>
-                value.map((user: User) => new UserOutputDto(user)),
-            );
+        const users: User[] = await this.userService.findFriends(user.login, query);
+        return users.map((user: User) => new UserOutputDto(user));
     }
 
     @Put('me/friends/:login')
@@ -122,7 +130,21 @@ export class UserController {
         @Identity() user: Identity,
         @Param('login') login: string,
     ): Promise<void> {
-        this.userService.addFriends(user.login, login);
+        const userOne: User | undefined = await this.userService.findOne(
+            user.login,
+        );
+        const userTwo: User | undefined = await this.userService.findOne(login);
+
+        if (!userOne) {
+            throw new NotFoundException(`user ${user.login} not found`);
+        }
+        if (!userTwo) {
+            throw new NotFoundException(`user ${login} not found`);
+        }
+
+        this.userService.addFriends(user.login, login).catch((error: Error) => {
+            throw new InternalServerErrorException(error.message);
+        });
     }
 
     @Delete('me/friends/:login')
@@ -131,7 +153,20 @@ export class UserController {
         @Identity() user: Identity,
         @Param('login') login: string,
     ): Promise<void> {
-        this.userService.removeFriends(user.login, login);
+        const userOne: User | undefined = await this.userService.findOne(
+            user.login,
+        );
+        const userTwo: User | undefined = await this.userService.findOne(login);
+
+        if (!userOne) {
+            throw new NotFoundException(`user ${user.login} not found`);
+        }
+        if (!userTwo) {
+            throw new NotFoundException(`user ${login} not found`);
+        }
+        this.userService.removeFriends(user.login, login).catch((error: Error) => {
+            throw new InternalServerErrorException(error.message);
+        });
     }
 
     @Get('me/avatars')
@@ -139,11 +174,21 @@ export class UserController {
         @Identity() user: Identity,
         @Query() query: QueryFilterDto,
     ): Promise<ActualAvatarOutputDto[]> {
+        const userFound: User | undefined = await this.userService.findOne(
+            user.login,
+        );
+        if (!userFound) {
+            throw new NotFoundException(`user ${user.login} not found`);
+        }
+
         return this.userService
             .listAvatars(user.login, query)
             .then((avatars: Avatar[]) =>
                 avatars.map((avatar: Avatar) => new AvatarOutputDto(avatar)),
-            );
+            )
+            .catch((error: Error) => {
+                throw new InternalServerErrorException(error.message);
+            });
     }
 
     @Delete('me/avatars/:avatar_id')
@@ -152,15 +197,36 @@ export class UserController {
         @Identity() user: Identity,
         @Param('avatar_id') id: number,
     ): Promise<void> {
-        this.userService
-            .listAvatars(user.login, new QueryFilterDto())
-            .then((avatars: Avatar[]) => {
-                if (avatars.length > 1) {
-                    this.userService.deleteAvatarByID(user.login, id);
-                } else {
-                    throw new BadRequestException('cannot delete the default avatar');
-                }
-            });
+        const userFound: User | undefined = await this.userService.findOne(
+            user.login,
+        );
+        if (!userFound) {
+            throw new NotFoundException(`user ${user.login} not found`);
+        }
+
+        const avatars: Avatar[] = await this.userService.listAvatars(
+            user.login,
+            new QueryFilterDto(),
+        );
+        if (avatars.length == 0) {
+            throw new InternalServerErrorException(
+                `no avatars for user ${user.login}`,
+            );
+        }
+        if (avatars.length == 1) {
+            throw new BadRequestException('cannot delete the default avatar');
+        }
+
+        const avatarFound: Avatar | undefined = avatars.find(
+            (value: Avatar): boolean => value.id == id,
+        );
+        if (!avatarFound) {
+            throw new NotFoundException(`avatar ${id} not found`);
+        }
+
+        this.userService.deleteAvatarByID(user.login, id).catch((error: Error) => {
+            throw new InternalServerErrorException(error.message);
+        });
     }
 
     @Get('me/match_history')
@@ -168,10 +234,19 @@ export class UserController {
         @Identity() user: Identity,
         @Query() query: QueryFilterDto,
     ): Promise<MatchOutputDto[]> {
+        const userFound: User | undefined = await this.userService.findOne(
+            user.login,
+        );
+        if (!userFound) {
+            throw new NotFoundException(`user ${user.login} not found`);
+        }
         return this.userService
             .listMatchByUserID(user.login, query)
             .then((matchs: Match[]) => {
                 return matchs.map((match: Match) => new MatchOutputDto(match));
+            })
+            .catch((error: Error) => {
+                throw new InternalServerErrorException(error.message);
             });
     }
 
--- user-api/src/user/user.service.ts
+++ user-api/src/user/user.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User, Avatar, Match } from 'db-interface/Core';
@@ -8,119 +7,119 @@ import { QueryFilterDto } from 'validation/query.dto';
 
 @Injectable()
 export class UserService {
-  constructor(
-    @InjectRepository(User)
-    private userRepository: Repository<User>,
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
 
-    @InjectRepository(Avatar)
-    private avatarRepository: Repository<Avatar>,
+        @InjectRepository(Avatar)
+        private avatarRepository: Repository<Avatar>,
 
-    @InjectRepository(Match)
-    private matchRepository: Repository<Match>,
-  ) {}
+        @InjectRepository(Match)
+        private matchRepository: Repository<Match>,
+    ) { }
 
-  create(login: string, avatarPath: string): Promise<User> {
-    let user = new User(login);
-    user.avatars = [new Avatar(avatarPath, true)];
-    return this.userRepository.save(user);
-  }
-
-  findAll(query: QueryFilterDto): Promise<User[]> {
-    return this.userRepository.find({
-      skip: query.onset,
-      take: query.length,
-      where: query.search
-        ? {
-            userName: Like(`%${query.search}%`),
-          }
-        : undefined,
-      relations: {
-        stats: true,
-      },
-    });
-  }
-
-  findOne(login: string): Promise<User> {
-    return this.userRepository.findOneBy({ login: login });
-  }
-
-  updateByLogin(
-    login: string,
-    updateUserDto: UpdateUserDto,
-  ): Promise<UpdateResult> {
-    return this.userRepository.update(
-      { login: login },
-      { userName: updateUserDto.username, twoFa: updateUserDto.two_fa },
-    );
-  }
-
-  removeByLogin(login: string): Promise<DeleteResult> {
-    return this.userRepository.delete({ login: login });
-  }
-
-  async findFriends(userLogin: string, query: QueryFilterDto): Promise<User[]> {
-    let user: User = await this.userRepository.findOne({
-      where: {
-        login: userLogin,
-      },
-      relations: {
-        friends: true,
-      },
-    });
-
-    let friends: User[] = user.friends.slice(query.onset, query.length);
-    if (query.search) {
-      return friends.map((value: User) =>
-        value.userName.includes(query.search) ? value : undefined,
-      );
+    create(login: string, avatarPath: string): Promise<User> {
+        let user = new User(login);
+        user.avatars = [new Avatar(avatarPath, true)];
+        return this.userRepository.save(user);
     }
-    return friends;
-  }
 
-  async addFriends(userLogin: string, friendLogin: string): Promise<void> {
-    await this.userRepository
-      .createQueryBuilder()
-      .relation(User, 'friends')
-      .of({ login: userLogin })
-      .add({ login: friendLogin });
-  }
+    findAll(query: QueryFilterDto): Promise<User[]> {
+        return this.userRepository.find({
+            skip: query.onset,
+            take: query.length,
+            where: query.search
+                ? {
+                    userName: Like(`%${query.search}%`),
+                }
+                : undefined,
+            relations: {
+                stats: true,
+            },
+        });
+    }
 
-  async removeFriends(userLogin: string, friendLogin: string): Promise<void> {
-    await this.userRepository
-      .createQueryBuilder()
-      .relation(User, 'friends')
-      .of({ login: userLogin })
-      .remove({ login: friendLogin });
-  }
+    findOne(login: string): Promise<User> {
+        return this.userRepository.findOneBy({ login: login });
+    }
 
-  async listAvatars(login: string, query: QueryFilterDto): Promise<Avatar[]> {
-    let user: User = await this.userRepository.findOne({
-      where: {
-        login: login,
-      },
-      relations: {
-        avatars: true,
-      },
-    });
+    updateByLogin(
+        login: string,
+        updateUserDto: UpdateUserDto,
+    ): Promise<UpdateResult> {
+        return this.userRepository.update(
+            { login: login },
+            { userName: updateUserDto.username, twoFa: updateUserDto.two_fa },
+        );
+    }
 
-    return user.avatars.slice(query.onset, query.length);
-  }
+    removeByLogin(login: string): Promise<DeleteResult> {
+        return this.userRepository.delete({ login: login });
+    }
 
-  async deleteAvatarByID(login: string, avatarID: number): Promise<void> {
-    this.avatarRepository.delete({ user: { login: login }, id: avatarID });
-  }
+    async findFriends(userLogin: string, query: QueryFilterDto): Promise<User[]> {
+        let user: User = await this.userRepository.findOne({
+            where: {
+                login: userLogin,
+            },
+            relations: {
+                friends: true,
+            },
+        });
 
-  async listMatchByUserID(
-    login: string,
-    query: QueryFilterDto,
-  ): Promise<Match[]> {
-    return this.matchRepository.find({
-      skip: query.onset,
-      take: query.length,
-      where: { participants: Equal(login) },
-      relations: {
-        participants: true,
-      },
-    });
-  }
+        let friends: User[] = user.friends.slice(query.onset, query.length);
+        if (query.search) {
+            return friends.map((value: User) =>
+                value.userName.includes(query.search) ? value : undefined,
+            );
+        }
+        return friends;
+    }
+
+    async addFriends(userLogin: string, friendLogin: string): Promise<void> {
+        await this.userRepository
+            .createQueryBuilder()
+            .relation(User, 'friends')
+            .of({ login: userLogin })
+            .add({ login: friendLogin });
+    }
+
+    async removeFriends(userLogin: string, friendLogin: string): Promise<void> {
+        await this.userRepository
+            .createQueryBuilder()
+            .relation(User, 'friends')
+            .of({ login: userLogin })
+            .remove({ login: friendLogin });
+    }
+
+    async listAvatars(login: string, query: QueryFilterDto): Promise<Avatar[]> {
+        let user: User = await this.userRepository.findOne({
+            where: {
+                login: login,
+            },
+            relations: {
+                avatars: true,
+            },
+        });
+
+        return user.avatars.slice(query.onset, query.length);
+    }
+
+    async deleteAvatarByID(login: string, avatarID: number): Promise<void> {
+        this.avatarRepository.delete({ user: { login: login }, id: avatarID });
+    }
+
+    async listMatchByUserID(
+        login: string,
+        query: QueryFilterDto,
+    ): Promise<Match[]> {
+        return this.matchRepository.find({
+            skip: query.onset,
+            take: query.length,
+            where: { participants: Equal(login) },
+            relations: {
+                participants: true,
+            },
+        });
+    }
 }
