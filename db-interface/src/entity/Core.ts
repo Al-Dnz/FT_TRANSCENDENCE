ad463b2f81fec2a994e05833c873fc03c84de063
AlexDNZ <paleozene@gmail.com>
2022-12-06 15:46:01 +0100

    build: update db model for User

--- db-interface/src/entity/Core.ts
+++ db-interface/src/entity/Core.ts
@@ -205,6 +205,10 @@ export class User extends Base {
     @Column("boolean", { default: false })
     twoFa: boolean;
 
+
+    @Column({ nullable: true, default: true })
+    socketId: string;
+
     @Column({ type: "enum", enum: UserStatus, default: UserStatus.offline })
     status: UserStatus;
 
