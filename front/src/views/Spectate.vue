c10323318e0f2cec63c4a7cbb94660e6ed52c3b7
AlexDNZ <paleozene@gmail.com>
2023-01-02 19:34:31 +0100

    update: sort match list by id

--- front/src/views/Spectate.vue
+++ front/src/views/Spectate.vue
@@ -63,7 +63,7 @@ export default defineComponent({
     this.socket.on('liveMatches', (payload : any) => {
       this.tab = [];
       this.tab = payload;
-      console.log(payload);
+      this.tab.sort(function (a: any, b: any) {return a.id - b.id });
     });
     this.socket.on('updateLiveMatches', async (payload : any) => {
       this.tab = [];
