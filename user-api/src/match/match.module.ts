5a0b410f103dc49de404b6c73dd693a172aad881
AlexDNZ <paleozene@gmail.com>
2023-01-04 15:22:19 +0100

    remove unfinished matches at deconnexion

--- user-api/src/match/match.module.ts
+++ user-api/src/match/match.module.ts
@@ -5,11 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlockerBlocked, Match, User } from 'db-interface/Core';
 import { UserService } from 'user-api/user.service';
 import {JwtModule } from '@nestjs/jwt';
-
+import { UserModule } from 'user-api/user.module';
 
 @Module({
     imports: [JwtModule.register({ secret: process.env.ACCESS_TOKEN_SECRET }), TypeOrmModule.forFeature([User, Match, BlockerBlocked])],
     controllers: [MatchController],
     providers: [MatchService, UserService],
+    exports: [MatchService],
 })
 export class MatchModule { }
