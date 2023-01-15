43f66912c43185b8365c6069cdc334df474a6dee
AlexDNZ <paleozene@gmail.com>
2023-01-15 14:08:52 +0100

    some changes in account display

--- front/src/components/HistoryBox.vue
+++ front/src/components/HistoryBox.vue
@@ -1,19 +1,21 @@
 <template>
-	<div :class=" results === true ? 'bg-green-400' : 'bg-red-400'" class="flex flex-row justify-between w-full h-full rounded-2xl" >
+	<div :class="results === true ? 'bg-green-400' : 'bg-red-400'"
+		class="flex flex-row justify-between w-full h-full rounded-2xl">
 		<div className="pl-4 h-full flex flex-col justify-center ">
-			<img :src="obj.playerOne.avatar.path" className ="h-20 w-20 rounded-xl hidden lg:block"/>
+			<img :src="obj.playerOne.avatar.path" className="h-20 w-20 rounded-xl hidden lg:block" />
 		</div>
 		<div className="flex flex-col justify-center overflow-hidden w-1/3 lg:w-1/5 ">
-			<span className="center-x">{{obj.playerOne.userName}} </span>
+			<span className="font-bold center-x">{{ obj.playerOne.userName }} </span>
 		</div>
 		<div className='flex flex-col justify-center overflow-hidden w-1/3 lg:w-1/5'>
-			<span className="center-x">{{obj.score1}} - {{obj.score2}}</span>
-		</div>	
+			<span className="center-x"> {{ displayDate(obj.createdAt) }} </span>
+			<span className="font-bold center-x">{{ obj.score1 }} - {{ obj.score2 }}</span>
+		</div>
 		<div className="flex flex-col justify-center overflow-hidden w-1/3 lg:w-1/5">
-			<span className="center-x">{{obj.playerTwo.userName}} </span>
+			<span className="font-bold center-x">{{ obj.playerTwo.userName }} </span>
 		</div>
 		<div className="pr-4 h-full flex flex-col justify-center ">
-			<img :src="obj.playerTwo.avatar.path" className ="h-20 w-20  rounded-xl hidden lg:block"/>
+			<img :src="obj.playerTwo.avatar.path" className="h-20 w-20  rounded-xl hidden lg:block" />
 		</div>
 	</div>
 </template>
@@ -21,17 +23,34 @@
 <script lang="ts">
 export default {
 	name: 'historyBox',
-	props : {
-		obj: {type: Object,
-			required: true},
-		index: {type : Number,
-			required: true},
-		results: {type: Boolean,
-			required: true}
+	methods:
+	{
+		displayDate(date: string): string 
+		{
+			let time = new Date(date);
+			let formatedDate = time.toLocaleString("en-GB");
+			return formatedDate;
+		}
 	},
-	methods: {
-	},
+	props: {
+		obj: {
+			type: Object,
+			required: true
+		},
+		index: {
+			type: Number,
+			required: true
+		},
+		results: {
+			type: Boolean,
+			required: true
+		}
+	}
 }
 </script>
   
+
+
+
+
 <style src="../assets/tailwind.css" />
